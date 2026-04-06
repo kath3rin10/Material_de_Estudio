8d34c032dfb57e87800fec77944f0e50f8189000
kath3 <katherindayanfrancorojas@gmail.com>
2026-04-06 11:06:35 -0500

    Add files via upload

---
+++ Trazabilidad_SGVA.sql
@@ -0,0 +1,73 @@
+CREATE DATABASE Trazabilidad_SGVA;
+USE Trazabilidad_SGVA;
+
+CREATE TABLE Trazabilidad(
+id_Trazabilidad INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+Codigo_empresa INT NOT NULL,
+encargo VARCHAR(45) NOT NULL,
+fecha_inicio DATE NOT NULL,
+fecha_fin DATE NOT NULL,
+telefono VARCHAR(15) NULL,
+correo VARCHAR(45) NULL,
+estado VARCHAR(45) NOT NULL,
+fecha_cierre datetime NULL
+   
+);
+
+CREATE TABLE Empresa(
+id_empresa INT AUTO_INCREMENT PRIMARY KEY,
+nombre_empresa VARCHAR(100) NOT NULL
+
+);
+
+DESCRIBE Empresa;
+INSERT INTO Empresa 
+VALUES('COD1', 'FIDUCIARIA LA PREVISORA S.A - FIDUPREVISORA S.A');
+
+INSERT INTO Empresa 
+VALUES('COD2','EKOOMEDIA S.A.S');
+
+INSERT INTO Empresa 
+VALUES('COD3','KOA COMPAÑIA DE FINANCIAMIENTO S.A-KOA CF');
+
+INSERT INTO Empresa 
+VALUE('COD4','NUEVA EMPRESA PROMOTORA DE SALUD S.A -NUEVA EPS S.A');
+
+INSERT INTO Empresa 
+VALUE('COD5','SOPORTE Y LOGISTICA S.A.S');
+
+INSERT INTO Empresa (Codigo_empresa, nombre_empresa)
+VALUES('COD1', 'FIDUCIARIA LA PREVISORA S.A - FIDUPREVISORA S.A');
+
+INSERT INTO Empresa (Codigo_empresa, nombre_empresa)
+VALUES('COD2','EKOOMEDIA S.A.S');
+
+INSERT INTO Empresa (Codigo_Empresa, nombre_empresa)
+VALUES('COD3','KOA COMPAÑIA DE FINANCIAMIENTO S.A-KOA CF');
+
+INSERT INTO Empresa (Codigo_Empresa, nombre_empresa)
+VALUE('COD4','NUEVA EMPRESA PROMOTORA DE SALUD S.A -NUEVA EPS S.A');
+
+INSERT INTO Empresa (Codigo_Empresa, nombre_empresa)
+VALUE('COD5','SOPORTE Y LOGISTICA S.A.S');
+
+INSERT INTO Empresa (Codigo_empresa, nombre_empresa)
+VALUES('COD1', 'FIDUCIARIA LA PREVISORA S.A - FIDUPREVISORA S.A'),
+('COD2','EKOOMEDIA S.A.S'),
+('COD3','KOA COMPAÑIA DE FINANCIAMIENTO S.A-KOA CF'),
+('COD4','NUEVA EMPRESA PROMOTORA DE SALUD S.A -NUEVA EPS S.A'),
+('COD5','SOPORTE Y LOGISTICA S.A.S');
+
+ALTER TABLE Empresa
+DROP COLUMN ID_EMPRESA;
+
+ALTER TABLE Empresa
+DROP id_Empresa;
+
+ALTER TABLE Empresa
+ADD PRIMARY KEY (codigo_empresa);
+
+DESCRIBE Empresa;
+
+ALTER TABLE Trazabilidad
+CHANGE Nombre_Empresa COD_Empresa varchar(50) not null;
